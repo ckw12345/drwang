d05d2122e16e06ab15f75f4c47baea4ec8aa6e4a
ckw12345 <113484783+ckw12345@users.noreply.github.com>
2026-03-29 15:52:19 -0700

    Update remove_refresh.py

--- remove_refresh.py
+++ remove_refresh.py
@@ -2,21 +2,21 @@ from bs4 import BeautifulSoup
 import os
 import glob
 
-# Folder where your HTML files are stored
-folder_path = "./"  # change to your folder path
+# Only look in casereports/ folder
+folder_path = "./casereports"
 
-# Find all files starting with 'acupuncture-for' and ending with .html
+# Find all HTML files starting with 'acupuncture-for'
 files = glob.glob(os.path.join(folder_path, "acupuncture-for*.html"))
 
 for file_path in files:
     with open(file_path, "r", encoding="utf-8") as f:
         soup = BeautifulSoup(f, "html.parser")
 
-    # Remove all <meta http-equiv="refresh"> tags
+    # Remove <meta http-equiv="refresh"> tags
     for meta in soup.find_all("meta", attrs={"http-equiv": "refresh"}):
         meta.decompose()
 
-    # Save back to the same file
+    # Save changes
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(str(soup))
 
